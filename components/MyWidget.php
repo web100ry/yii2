25d06032fa3198062c70af5f32198f3afda22696
web100ry <valerij.ivanjuk@gmail.com>
2018-04-11 00:05:17 +0300

    Lessons 18 Widget

---
+++ components/MyWidget.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: root
+ * Date: 10.04.18
+ * Time: 23:21
+ */
+class MyWidget
+{
+
+}
---
+++ components/views/my.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: root
+ * Date: 10.04.18
+ * Time: 23:38
+ */
